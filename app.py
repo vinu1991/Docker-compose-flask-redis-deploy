97119c6f4a53380bb5b013b2d7e5ce68360d979e
vinu1991 <s.vinu1991@gmail.com>
2020-11-10 17:37:45 +0530

    My Name at Docker

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Vinu\'s Docker! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
